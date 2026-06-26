a2218c809749d4ac16a37735efd6cb2f9660d292
wawaxgz <144407509+wawaxgz@users.noreply.github.com>
2026-06-26 10:43:24 +0800

    Create work.js

---
+++ data/work.js
@@ -0,0 +1,26 @@
+[
+  {
+    "name": "濟州島街道",
+    "price": 20,
+    "category": "韓國",
+    "camera": "C34EFJ",
+    "film": "Fujifilm C200",
+    "image": "https://raw.githubusercontent.com/wawaxgz/photography-portfolio/main/assets/img/street2.jpeg"
+  },
+  {
+    "name": "城山日出峰",
+    "price": 50,
+    "category": "韓國",
+    "camera": "C35EFJ",
+    "film": "Fujifilm C200",
+    "image": "https://raw.githubusercontent.com/wawaxgz/photography-portfolio/main/assets/img/songsanpeak.jpeg"
+  },
+  {
+    "name": "濟州島街頭",
+    "price": 20,
+    "category": "韓國",
+    "camera": "C35EFJ",
+    "film": "Fujifilm C200",
+    "image": "https://raw.githubusercontent.com/wawaxgz/photography-portfolio/main/assets/img/street1.jpeg"
+  }
+]
